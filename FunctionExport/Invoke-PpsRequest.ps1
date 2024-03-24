34643450aaddac28d17b9ad8dee83e7b9ccee4a2
capnevyn <cap.nevyn@gmail.com>
2024-03-25 09:38:39 +1300

    Added a new Function

---
+++ FunctionExport/Invoke-PpsRequest.ps1
@@ -0,0 +1,109 @@
+function Invoke-PpsRequest
+{
+    <#
+        .SYNOPSIS
+            Invoke request against Pleasant Password Server
+
+        .DESCRIPTION
+            Invoke request against Pleasant Password Server
+
+        .PARAMETER Uri
+            xxx
+
+        .PARAMETER Method
+            xxx
+
+        .PARAMETER Data
+            xxx
+
+        .PARAMETER Session
+            Makes it possible to connect to multiple Pleasant Password Servers
+
+        .EXAMPLE
+            xxx
+    #>
+
+    [CmdletBinding()]
+    [OutputType([PSCustomObject])]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Uri,
+
+        [Parameter()]
+        [Microsoft.PowerShell.Commands.WebRequestMethod]
+        $Method = 'Get',
+
+        [Parameter()]
+        [object]
+        $Data,
+
+        [Parameter()]
+        [string]
+        $Session = 'Default'
+    )
+
+    begin
+    {
+        Write-Verbose -Message "Begin (ErrorActionPreference: $ErrorActionPreference)"
+        $origErrorActionPreference = $ErrorActionPreference
+        $verbose = $PSBoundParameters.ContainsKey('Verbose') -or ($VerbosePreference -ne 'SilentlyContinue')
+
+        # FIXXXME - should this be moved out - so it is run when module is imported?
+        if (-not $script:SessionList)
+        {
+            Write-Verbose -Message 'Creating session list'
+            $script:SessionList = @{}
+        }
+    }
+
+    process
+    {
+        Write-Verbose -Message "Process begin (ErrorActionPreference: $ErrorActionPreference)"
+
+        try
+        {
+            # Make sure that we don't continue on error, and that we catches the error
+            $ErrorActionPreference = 'Stop'
+
+            if (-not ($s = $script:SessionList[$Session]))
+            {
+                throw "Session <$Session> not found"
+            }
+
+            $requestParams = @{
+                Method          = $Method
+                Uri             = "$($s.Uri)/$Uri"
+                Headers         = $s.Headers
+                UseBasicParsing = $true
+            }
+            if ($Data)
+            {
+                $requestParams['Body']        = ConvertTo-Json -Compress -Depth 9 -InputObject $Data
+                $requestParams['ContentType'] = 'application/json; charset=utf-8'
+            }
+
+            Write-Verbose -Message ($requestParams | ConvertTo-Json)
+
+            # Return
+            Invoke-RestMethod @requestParams
+        }
+        catch
+        {
+            Write-Verbose -Message "Encountered an error: $_"
+            Write-Error -ErrorAction $origErrorActionPreference -Exception $_.Exception
+        }
+        finally
+        {
+            $ErrorActionPreference = $origErrorActionPreference
+        }
+
+        Write-Verbose -Message 'Process end'
+    }
+
+    end
+    {
+        Write-Verbose -Message 'End'
+    }
+}
